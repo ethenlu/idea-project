6923e4b12b6f0459eacfe67443c4be9fbb0a872c
ethenlu <ethenlu@163.com>
2020-09-24 11:20:06 +0800

    提交一个idea项目

---
+++ git/src/main/java/com/example/utils/Project.java
@@ -0,0 +1,14 @@
+package com.example.utils;
+
+/*
+@author:Ethenlu
+*/
+public class Project {
+    public static void main(String[] args) {
+        System.out.println("ssss");
+        System.out.println("ssss");
+        System.out.println("ssss");
+        System.out.println("ssss");
+        System.out.println("ssss");
+    }
+}
